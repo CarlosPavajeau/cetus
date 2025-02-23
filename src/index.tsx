c8c2d50ca3bf177329d0508b938e41e09c2ecc42
CarlosPavajeau <cantte098@gmail.com>
2025-02-22 19:21:37 -0500

    feat: add routing configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
+import { RouterProvider, createRouter } from '@tanstack/react-router'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 
-const rootEl = document.getElementById('root')
-if (rootEl) {
-  const root = ReactDOM.createRoot(rootEl)
+import { routeTree } from './routeTree.gen'
+
+const router = createRouter({ routeTree })
+
+const rootElement = document.getElementById('root')
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement)
   root.render(
     <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </React.StrictMode>,
   )
 }
---
+++ src/routes/__root.tsx
@@ -0,0 +1,27 @@
+import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+
+import * as css from '@/styles/index.css?url'
+
+const RootComponent = () => {
+  return (
+    <div>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </div>
+  )
+}
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        title: 'cetus',
+      },
+    ],
+    link: [{ rel: 'stylesheet', href: css }],
+  }),
+  component: RootComponent,
+})
