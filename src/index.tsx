7efff10341daaac3749907cc11faf5283ae43d7a
CarlosPavajeau <cantte098@gmail.com>
2025-02-22 19:27:25 -0500

    feat: add clerk provider for authentication

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,25 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { ClerkProvider } from '@clerk/clerk-react'
 import { routeTree } from './routeTree.gen'
 
 const router = createRouter({ routeTree })
+
+const PUBLISHABLE_KEY = import.meta.env.PUBLIC_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 const rootElement = document.getElementById('root')
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <RouterProvider router={router} />
+      </ClerkProvider>
     </React.StrictMode>,
   )
 }
