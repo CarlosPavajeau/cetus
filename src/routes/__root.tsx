c9524a78864c4ac6f29293bbbaee050936feeb3c
CarlosPavajeau <cantte098@gmail.com>
2025-02-23 16:12:50 -0500

    feat: update root component layout with container styling

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,7 +5,7 @@ import * as css from '@/styles/index.css?url'
 
 const RootComponent = () => {
   return (
-    <div>
+    <div className='container mx-auto'>
       <Outlet />
       <TanStackRouterDevtools />
     </div>
