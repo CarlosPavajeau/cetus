263c2af9e913de97f94238aa6c465f9de34c0d64
CarlosPavajeau <cantte098@gmail.com>
2025-02-23 16:08:09 -0500

    feat: implement categories display in index page with loading and error handling

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { useCategories } from '@/hooks/use-categories'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
@@ -5,5 +6,24 @@ export const Route = createFileRoute('/')({
 })
 
 function IndexPage() {
-  return <div>Index Page</div>
+  const { categories, isLoading, error } = useCategories()
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>
+  }
+
+  return (
+    <div className="container mx-auto">
+      <h1 className="font-bold text-3xl">Categorias</h1>
+      <ul>
+        {categories?.map((category) => (
+          <li key={category.id}>{category.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
