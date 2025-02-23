106b3845212c342b2f4456da5b80c16606b53fd3
CarlosPavajeau <cantte098@gmail.com>
2025-02-23 17:21:20 -0500

    feat: enhance app navigation layout and add home link

--- src/components/app-nav.tsx
+++ src/components/app-nav.tsx
@@ -10,10 +10,15 @@ export const AppNav = () => {
   return (
     <header className="mb-10 items-center border-border border-b">
       <div className="container mx-auto flex items-center justify-between p-4">
-        <div className="flex items-center space-x-4">
-          <h1 className="font-bold text-xl">Cetus</h1>
+        <div className="flex items-center space-x-8">
+          <div>
+            <h1 className="font-bold text-xl">Cetus</h1>
+          </div>
 
-          <Link to="/app/products/new">Crear producto</Link>
+          <div className="flex space-x-4">
+            <Link to="/app">Inicio</Link>
+            <Link to="/app/products/new">Crear producto</Link>
+          </div>
         </div>
         <div>
           <SignedIn>
