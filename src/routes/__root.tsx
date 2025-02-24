92cbe2e2b3ef81bdbe82cdbf045d393da152c2d0
CarlosPavajeau <cantte098@gmail.com>
2025-02-24 08:46:17 -0500

    feat: refactor RootComponent layout for improved structure and readability

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,13 +5,12 @@ import * as css from '@/styles/index.css?url'
 
 const RootComponent = () => {
   return (
-    <>
-      <div className="before:-left-12 after:-right-12 relative mx-auto w-full max-w-6xl before:absolute before:inset-y-0 before:w-px before:bg-[linear-gradient(to_bottom,--theme(--color-border/.3),--theme(--color-border)_200px,--theme(--color-border)_calc(100%-200px),--theme(--color-border/.3))] after:absolute after:inset-y-0 after:w-px after:bg-[linear-gradient(to_bottom,--theme(--color-border/.3),--theme(--color-border)_200px,--theme(--color-border)_calc(100%-200px),--theme(--color-border/.3))]"></div>
-      <main className="relative flex min-h-screen flex-col">
+    <div className="before:-left-12 after:-right-12 relative mx-auto w-full max-w-6xl before:absolute before:inset-y-0 before:w-px before:bg-[linear-gradient(to_bottom,--theme(--color-border/.3),--theme(--color-border)_200px,--theme(--color-border)_calc(100%-200px),--theme(--color-border/.3))] after:absolute after:inset-y-0 after:w-px after:bg-[linear-gradient(to_bottom,--theme(--color-border/.3),--theme(--color-border)_200px,--theme(--color-border)_calc(100%-200px),--theme(--color-border/.3))]">
+      <div className="relative flex min-h-screen flex-col">
         <Outlet />
         <TanStackRouterDevtools />
-      </main>
-    </>
+      </div>
+    </div>
   )
 }
 
