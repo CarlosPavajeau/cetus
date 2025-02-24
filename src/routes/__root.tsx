b5949ec0a345ed4509c5e18ea7087ad538de47b4
CarlosPavajeau <cantte098@gmail.com>
2025-02-23 23:14:30 -0500

    feat: enhance layout structure in RootComponent for improved styling and responsiveness

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,10 +5,13 @@ import * as css from '@/styles/index.css?url'
 
 const RootComponent = () => {
   return (
-    <main className="min-h-screen">
-      <Outlet />
-      <TanStackRouterDevtools />
-    </main>
+    <>
+      <div className="before:-left-12 after:-right-12 relative mx-auto w-full max-w-6xl before:absolute before:inset-y-0 before:w-px before:bg-[linear-gradient(to_bottom,--theme(--color-border/.3),--theme(--color-border)_200px,--theme(--color-border)_calc(100%-200px),--theme(--color-border/.3))] after:absolute after:inset-y-0 after:w-px after:bg-[linear-gradient(to_bottom,--theme(--color-border/.3),--theme(--color-border)_200px,--theme(--color-border)_calc(100%-200px),--theme(--color-border/.3))]"></div>
+      <main className="relative flex min-h-screen flex-col">
+        <Outlet />
+        <TanStackRouterDevtools />
+      </main>
+    </>
   )
 }
 
