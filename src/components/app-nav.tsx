0a1daed37ceff1e5e7855d5f4bb46ffe4ed250b5
CarlosPavajeau <cantte098@gmail.com>
2025-02-23 23:14:40 -0500

    feat: refactor AppNav component for improved layout and accessibility

--- src/components/app-nav.tsx
+++ src/components/app-nav.tsx
@@ -8,19 +8,31 @@ import { Link } from '@tanstack/react-router'
 
 export const AppNav = () => {
   return (
-    <header className="mb-10 items-center border-border border-b">
-      <div className="container mx-auto flex items-center justify-between p-4">
-        <div className="flex items-center space-x-8">
-          <div>
-            <h1 className="font-bold text-xl">Cetus</h1>
-          </div>
+    <header className="before:-inset-x-32 relative mb-14 before:absolute before:bottom-0 before:h-px before:bg-[linear-gradient(to_right,--theme(--color-border/.3),--theme(--color-border)_200px,--theme(--color-border)_calc(100%-200px),--theme(--color-border/.3))]">
+      <div
+        className="before:-bottom-px before:-left-12 before:-ml-px after:-right-12 after:-bottom-px after:-mr-px before:absolute before:z-10 before:size-[3px] before:bg-ring/50 after:absolute after:z-10 after:size-[3px] after:bg-ring/50"
+        aria-label="hidden"
+      ></div>
 
-          <div className="flex space-x-4">
-            <Link to="/app">Inicio</Link>
-            <Link to="/app/products/new">Crear producto</Link>
-          </div>
-        </div>
-        <div>
+      <div className="mx-auto flex h-[72px] w-full max-w-6xl items-center justify-between gap-3">
+        <Link to="/" className="flex items-center gap-2">
+          <h1 className="font-bold font-heading text-2xl text-foreground">
+            Cetus
+          </h1>
+        </Link>
+
+        <div className="flex items-center gap-4 sm:gap-8">
+          <Link to="/app" className="flex items-center gap-2">
+            <span className="inline-flex gap-0.5 text-sm hover:underline">
+              Inicio
+            </span>
+          </Link>
+          <Link to="/app/products/new" className="flex items-center gap-2">
+            <span className="inline-flex gap-0.5 text-sm hover:underline">
+              Crear producto
+            </span>
+          </Link>
+
           <SignedIn>
             <UserButton />
           </SignedIn>
