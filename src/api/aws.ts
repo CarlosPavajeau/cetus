c1df8518021e1fc5e8b1887d02b6155b1c6b188e
CarlosPavajeau <cantte098@gmail.com>
2025-02-28 14:23:16 -0500

    feat: add file upload functionality to AWS S3 with signed URLs

--- src/api/aws.ts
+++ src/api/aws.ts
@@ -16,3 +16,30 @@ export const createSignedUrl = async (request: CreateSignedUrlRequest) => {
 
   return response.data
 }
+
+export type UploadFileRequest = {
+  url: string
+  file: File
+}
+
+export const uploadFile = async (request: UploadFileRequest) => {
+  const response = await axios.put(request.url, request.file, {
+    headers: {
+      'Content-Type': request.file.type,
+    },
+  })
+
+  return response
+}
+
+export const uploadFileToS3 = async (fileName: string, file: File) => {
+  const { url } = await createSignedUrl({ fileName })
+
+  await uploadFile({ url, file })
+}
+
+export const uploadFilesToS3 = async (files: FileList) => {
+  const fileArray = Array.from(files)
+
+  await Promise.all(fileArray.map((file) => uploadFileToS3(file.name, file)))
+}
