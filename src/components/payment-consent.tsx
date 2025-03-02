337ed966477ad7882acb8e8093145dfab29736b1
CarlosPavajeau <cantte098@gmail.com>
2025-03-01 22:29:13 -0500

    refactor: simplify payment consent component structure and improve readability

--- src/components/payment-consent.tsx
+++ src/components/payment-consent.tsx
@@ -10,83 +10,79 @@ export const PaymentConsent = () => {
   const { merchant } = useMerchant()
   const form = useFormContext<PaymentFormValues>()
 
+  if (!merchant) return null
+
   return (
-    <>
-      {merchant && (
-        <div className="space-y-4">
-          <FormField
-            control={form.control}
-            name="presigned_acceptance"
-            render={({ field }) => (
-              <FormItem>
-                <div className="flex items-center gap-2">
-                  <Checkbox
-                    checked={field.value}
-                    onCheckedChange={field.onChange}
-                  />
-                  <Label
-                    htmlFor="presigned_acceptance"
-                    className="text-muted-foreground"
-                  >
-                    Acepto haber leído{' '}
-                    <a
-                      href={merchant.data.presigned_acceptance.permalink}
-                      className="text-foreground underline"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      los reglamentos
-                    </a>{' '}
-                    para hacer este pago.
-                  </Label>
-                </div>
+    <div className="space-y-4">
+      <FormField
+        control={form.control}
+        name="presigned_acceptance"
+        render={({ field }) => (
+          <FormItem>
+            <div className="flex items-center gap-2">
+              <Checkbox
+                checked={field.value}
+                onCheckedChange={field.onChange}
+              />
+              <Label
+                htmlFor="presigned_acceptance"
+                className="text-muted-foreground"
+              >
+                Acepto haber leído{' '}
+                <a
+                  href={merchant.data.presigned_acceptance.permalink}
+                  className="text-foreground underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  los reglamentos
+                </a>{' '}
+                para hacer este pago.
+              </Label>
+            </div>
 
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+            <FormMessage />
+          </FormItem>
+        )}
+      />
 
-          <FormField
-            control={form.control}
-            name="presigned_personal_data_auth"
-            render={({ field }) => (
-              <FormItem>
-                <div className="flex items-center gap-2">
-                  <Checkbox
-                    checked={field.value}
-                    onCheckedChange={field.onChange}
-                  />
-                  <Label
-                    htmlFor="presigned_acceptance"
-                    className="text-muted-foreground"
-                  >
-                    Acepto la{' '}
-                    <a
-                      href={
-                        merchant.data.presigned_personal_data_auth.permalink
-                      }
-                      className="text-foreground underline"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      autorización para la administración de datos personales
-                    </a>
-                  </Label>
-                </div>
+      <FormField
+        control={form.control}
+        name="presigned_personal_data_auth"
+        render={({ field }) => (
+          <FormItem>
+            <div className="flex items-center gap-2">
+              <Checkbox
+                checked={field.value}
+                onCheckedChange={field.onChange}
+              />
+              <Label
+                htmlFor="presigned_acceptance"
+                className="text-muted-foreground"
+              >
+                Acepto la{' '}
+                <a
+                  href={merchant.data.presigned_personal_data_auth.permalink}
+                  className="text-foreground underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  autorización para la administración de datos personales
+                </a>
+              </Label>
+            </div>
 
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+            <FormMessage />
+          </FormItem>
+        )}
+      />
 
-          <div className="flex items-center gap-2">
-            <ShieldCheckIcon className="text-foreground" size={16} />
-            <p className="text-muted-foreground text-xs">
-              Pagos seguros con Wompi.
-            </p>
-          </div>
-        </div>
-      )}
-    </>
+      <div className="flex items-center gap-2">
+        <ShieldCheckIcon className="text-foreground" size={16} />
+        <p className="text-muted-foreground text-xs">
+          Pagos seguros con Wompi.
+        </p>
+      </div>
+    </div>
   )
 }
