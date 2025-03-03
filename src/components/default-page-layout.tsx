dc03af8714732363e55675134862548e8fbfc749
CarlosPavajeau <cantte098@gmail.com>
2025-03-03 11:17:31 -0500

    fix: update accessibility attributes and clean up CSS variable definitions

--- src/components/default-page-layout.tsx
+++ src/components/default-page-layout.tsx
@@ -20,7 +20,7 @@ export function DefaultPageLayout({
         <header className="before:-inset-x-32 relative mb-14 before:absolute before:bottom-0 before:h-px before:bg-[linear-gradient(to_right,--theme(--color-border/.3),--theme(--color-border)_200px,--theme(--color-border)_calc(100%-200px),--theme(--color-border/.3))]">
           <div
             className="before:-bottom-px before:-left-12 before:-ml-px after:-right-12 after:-bottom-px after:-mr-px before:absolute before:z-10 before:size-[3px] before:bg-ring/50 after:absolute after:z-10 after:size-[3px] after:bg-ring/50"
-            aria-label="hidden"
+            aria-hidden="true"
           ></div>
 
           <div className="mx-auto flex h-[72px] w-full max-w-6xl items-center justify-between gap-3">
@@ -55,7 +55,7 @@ export function DefaultPageLayout({
         </header>
       )}
       <main className="grow">
-        <div data-home="true">{children}</div>
+        <div>{children}</div>
       </main>
     </>
   )
