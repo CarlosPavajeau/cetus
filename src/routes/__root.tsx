b66b86d4b78a815af7db57771c888611f9d02e13
cpavajea <cpavajea@emeal.nttdata.com>
2025-03-30 11:13:18 -0500

    refactor: remove title from index.html and integrate HeadContent component in RootComponent for improved SEO

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { Toaster } from '@/components/ui/sonner'
 import { TooltipProvider } from '@/components/ui/tooltip'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { HeadContent, Outlet, createRootRoute } from '@tanstack/react-router'
 
 import React, { Suspense } from 'react'
 
@@ -19,10 +19,7 @@ const RootComponent = () => {
   return (
     <div className="before:-left-12 after:-right-12 relative mx-auto w-full max-w-6xl before:absolute before:inset-y-0 before:w-px before:bg-[linear-gradient(to_bottom,--theme(--color-border/.3),--theme(--color-border)_200px,--theme(--color-border)_calc(100%-200px),--theme(--color-border/.3))] after:absolute after:inset-y-0 after:w-px after:bg-[linear-gradient(to_bottom,--theme(--color-border/.3),--theme(--color-border)_200px,--theme(--color-border)_calc(100%-200px),--theme(--color-border/.3))]">
       <div className="relative flex min-h-screen flex-col">
-        <meta
-          name="keywords"
-          content="TELEDIGITAL JYA, ecommerce, store, shop, products"
-        />
+        <HeadContent />
         <TooltipProvider>
           <Outlet />
           <Toaster />
