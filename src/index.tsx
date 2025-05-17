74037e93faafa59ba63c80621305f7c06292de74
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-05-17 12:25:21 -0500

    fix: enable scroll restoration in router configuration for improved navigation

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ import { routeTree } from './routeTree.gen'
 
 import './styles/index.css'
 
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree, scrollRestoration: true })
 
 declare module '@tanstack/react-router' {
   interface Register {
