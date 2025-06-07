a232b26cc699d5aa7151d02a05ab160a757b83c8
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-06-06 23:45:28 -0500

    feat: enhance InsightCard component to display percentage change alongside value, improving insights clarity and layout

--- src/components/order/orders-insights.tsx
+++ src/components/order/orders-insights.tsx
@@ -1,35 +1,44 @@
 import { Currency } from '@/components/currency'
-import { Card, CardContent } from '@/components/ui/card'
 import { Skeleton } from '@/components/ui/skeleton'
 import { useOrderInsights } from '@/hooks/orders'
+import { cn } from '@/shared/cn'
 import { useSearch } from '@tanstack/react-router'
-import {
-  DollarSignIcon,
-  type LucideIcon,
-  PackageIcon,
-  ReceiptIcon,
-  ShoppingCartIcon,
-} from 'lucide-react'
+import { useNumberFormatter } from 'react-aria'
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
 
 type InsightCardProps = {
   title: string
   value: string | number | React.ReactNode
-  icon: LucideIcon
 }
 
-export function InsightCard({ title, value, icon }: InsightCardProps) {
-  const CardIcon = icon as LucideIcon
-  return (
-    <Card className="overflow-hidden rounded-md py-0">
-      <CardContent className="flex items-center gap-4 p-4">
-        <div className="flex size-10 shrink-0 items-center justify-center rounded-full bg-primary/10 max-[480px]:hidden dark:bg-primary">
-          <CardIcon className="size-5 text-primary dark:text-primary-foreground" />
-        </div>
+export function InsightCard({ title, value }: InsightCardProps) {
+  const percentageFormatter = useNumberFormatter({
+    style: 'percent',
+    minimumFractionDigits: 1,
+    maximumFractionDigits: 1,
+    signDisplay: 'always',
+  })
 
-        <div>
-          <p className="font-medium text-muted-foreground text-sm">{title}</p>
-          <p className="font-semibold text-2xl">{value}</p>
-        </div>
+  // TODO: calculate percentage change
+  const percentageChange = Math.random() * 0.01 - 0.005
+
+  return (
+    <Card className="w-full gap-0 overflow-hidden">
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+      <CardContent className="mt-1 flex items-center gap-1.5">
+        <span className="font-medium text-2xl">{value}</span>
+        <span className="text-muted-foreground text-xs">
+          <span
+            className={cn(
+              percentageChange > 0 ? 'text-success-base' : 'text-destructive',
+            )}
+          >
+            {percentageFormatter.format(percentageChange)}
+          </span>{' '}
+          vs mes anterior
+        </span>
       </CardContent>
     </Card>
   )
@@ -44,8 +53,7 @@ export function OrdersInsights() {
 
   if (isLoading) {
     return (
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-        <Skeleton className="h-[100px] w-full" />
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         <Skeleton className="h-[100px] w-full" />
         <Skeleton className="h-[100px] w-full" />
         <Skeleton className="h-[100px] w-full" />
@@ -61,34 +69,21 @@ export function OrdersInsights() {
     {
       title: 'Pedidos',
       value: insights.allOrdersCount,
-      icon: PackageIcon,
-    },
-    {
-      title: 'Pedidos completados',
-      value: insights.completedOrdersCount,
-      icon: ShoppingCartIcon,
     },
     {
       title: 'Ingresos del mes',
       value: <Currency value={insights.currentMonthTotal} currency="COP" />,
-      icon: DollarSignIcon,
     },
     {
       title: 'Costo del mes',
       value: <Currency value={insights.currentMonthCost} currency="COP" />,
-      icon: ReceiptIcon,
     },
   ]
 
   return (
-    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
       {insightsData.map((item) => (
-        <InsightCard
-          key={item.title}
-          title={item.title}
-          value={item.value}
-          icon={item.icon}
-        />
+        <InsightCard key={item.title} title={item.title} value={item.value} />
       ))}
     </div>
   )
