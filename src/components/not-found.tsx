e79efcaa1271368baeace8c155f651e6c7b76aeb
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-07-06 13:24:33 -0500

    chore: format code

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -1,7 +1,3 @@
 export function NotFound() {
-    return (
-        <h1 className="font-heading">
-            No encontrado
-        </h1>
-    )
+  return <h1 className="font-heading">No encontrado</h1>
 }
