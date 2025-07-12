e515a40bf4e0b8f57272ac365052b5929a452cc0
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-07-11 23:48:22 -0500

    refactor: improve SuggestedProducts layout for better readability and styling

--- src/components/product/suggested-product.tsx
+++ src/components/product/suggested-product.tsx
@@ -11,8 +11,8 @@ export function SuggestedProducts({ products }: Props) {
   }
 
   return (
-    <div>
-      <h2 className="mb-6 font-bold text-xl">Productos sugeridos</h2>
+    <div className="flex flex-col gap-6">
+      <h2 className="font-heading font-medium text-3xl">Productos sugeridos</h2>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
