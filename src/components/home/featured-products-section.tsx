32a6a245c26095ad478113fd4d799d1af9102d7b
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-07-12 11:20:43 -0500

    feat: add FeaturedProductsSection component for displaying highlighted products

---
+++ src/components/home/featured-products-section.tsx
@@ -0,0 +1,55 @@
+import type { ProductForSale } from '@/api/products'
+import { ProductCard } from '@/components/product/product-card'
+import { Button } from '@/components/ui/button'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from '@/components/ui/carousel'
+import Autoplay from 'embla-carousel-autoplay'
+import { ArrowRightIcon } from 'lucide-react'
+import { useRef } from 'react'
+
+type Props = {
+  products: ProductForSale[]
+}
+
+export function FeaturedProductsSection({ products }: Props) {
+  const plugin = useRef(Autoplay({ delay: 2000, stopOnInteraction: false }))
+
+  return (
+    <div className="flex flex-col items-center gap-6 pb-6">
+      <p className="w-full text-left font-heading font-medium text-2xl">
+        Productos destacados
+      </p>
+
+      <div className="w-full">
+        <Carousel
+          plugins={[plugin.current]}
+          className="w-full"
+          onMouseEnter={plugin.current.stop}
+          onMouseLeave={plugin.current.reset}
+        >
+          <CarouselContent className="-ml-1">
+            {products.map((product) => (
+              <CarouselItem key={product.id} className="basis-1/5 pl-4">
+                <ProductCard key={product.id} product={product} />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </div>
+
+      <div className="flex">
+        <Button size="lg" className="group">
+          Ver todos
+          <ArrowRightIcon
+            className="-me-1 opacity-60 transition-transform group-hover:translate-x-0.5"
+            size={16}
+            aria-hidden="true"
+          />
+        </Button>
+      </div>
+    </div>
+  )
+}
