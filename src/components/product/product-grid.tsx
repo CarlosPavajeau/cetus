f46e8a5251cad012924675aaaf810ad08d2de2b2
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-07-11 23:13:33 -0500

    refactor: enhance ProductGrid layout for improved responsiveness and alignment

--- src/components/product/product-grid.tsx
+++ src/components/product/product-grid.tsx
@@ -24,7 +24,7 @@ export function ProductGrid({ products }: Props) {
   }
 
   return (
-    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <div className="grid w-full grid-cols-1 flex-col items-center gap-6 pb-14 md:grid-cols-3 lg:grid-cols-4">
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
