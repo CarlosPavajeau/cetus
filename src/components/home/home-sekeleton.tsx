d33a5001eb6d3ac468e33b208647e49b9bc2d46e
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-07-12 11:35:22 -0500

    feat: add HomeSkeleton component for displaying loading states of featured and popular products

---
+++ src/components/home/home-sekeleton.tsx
@@ -0,0 +1,27 @@
+import { DefaultPageLayout } from '@/components/default-page-layout'
+import { HeroSection } from '@/components/home/hero-section'
+import { ProductGridSkeleton } from '@/components/product/product-grid-skeleton'
+
+export function HomeSkeleton() {
+  return (
+    <DefaultPageLayout>
+      <HeroSection />
+
+      <div className="flex flex-col items-center gap-6 pb-6">
+        <p className="w-full text-left font-heading font-medium text-2xl">
+          Productos destacados
+        </p>
+
+        <ProductGridSkeleton />
+      </div>
+
+      <div className="my-8 flex flex-col items-center gap-6 rounded bg-muted p-4">
+        <p className="w-full text-center font-heading font-medium text-3xl">
+          Productos populares
+        </p>
+
+        <ProductGridSkeleton />
+      </div>
+    </DefaultPageLayout>
+  )
+}
