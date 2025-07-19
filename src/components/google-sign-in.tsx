831c8a47ac7e0f5b2b0fbbeb883c4bb137858e61
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-07-18 22:05:41 -0500

    feat: implement GoogleSignIn component for social authentication

---
+++ src/components/google-sign-in.tsx
@@ -0,0 +1,32 @@
+import { authClient } from '@/auth/auth-client'
+import { GoogleIcon } from '@/components/icons/google'
+import { Button } from '@/components/ui/button'
+import { useState } from 'react'
+
+export function GoogleSignIn() {
+  const [isLoading, setLoading] = useState(false)
+
+  const signIn = async () => {
+    setLoading(true)
+    await authClient.signIn.social({
+      provider: 'google',
+      callbackURL: '/app',
+    })
+
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  }
+  return (
+    <Button
+      size="lg"
+      variant="outline"
+      className="w-full"
+      onClick={signIn}
+      disabled={isLoading}
+    >
+      <GoogleIcon className="h-5 w-5" />
+      <span className="whitespace-nowrap">Continuar con Google</span>
+    </Button>
+  )
+}
