c6066731493f1f9007f61074b5421000d3910dcc
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-07-19 11:32:32 -0500

    feat: implement axios instance with auth token interceptor

---
+++ src/api/client.ts
@@ -0,0 +1,20 @@
+import { GetAuthToken } from '@/server/get-auth-token'
+import { API_ENDPOINT } from '@/shared/constants'
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: API_ENDPOINT,
+})
+
+api.interceptors.request.use(async (config) => {
+    const response = await GetAuthToken()
+
+    if (response) {
+        config.headers.Authorization = `Bearer ${response.token}`
+    }
+
+    return config
+})
+
+export { api }
+
