23a3d71fc7abcb7296989faf2468b50e6bfca850
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-07-19 12:18:04 -0500

    chore: format code

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,18 +3,17 @@ import { API_ENDPOINT } from '@/shared/constants'
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: API_ENDPOINT,
+  baseURL: API_ENDPOINT,
 })
 
 api.interceptors.request.use(async (config) => {
-    const response = await GetAuthToken()
+  const response = await GetAuthToken()
 
-    if (response) {
-        config.headers.Authorization = `Bearer ${response.token}`
-    }
+  if (response) {
+    config.headers.Authorization = `Bearer ${response.token}`
+  }
 
-    return config
+  return config
 })
 
 export { api }
-
