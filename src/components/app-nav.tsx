4e22190480c14e35c8122f58cb6f16192f095a46
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-07-19 12:29:24 -0500

    feat: update AppNav to use user context and remove organization switcher

--- src/components/app-nav.tsx
+++ src/components/app-nav.tsx
@@ -1,9 +1,13 @@
 import { ThemeSwitch } from '@/components/theme-switch'
 import { Separator } from '@/components/ui/separator'
 import { SidebarTrigger } from '@/components/ui/sidebar'
-import { OrganizationSwitcher, UserButton } from '@clerk/tanstack-react-start'
+import { useRouteContext } from '@tanstack/react-router'
 
 export const AppNav = () => {
+  const { user } = useRouteContext({
+    from: '/app',
+  })
+
   return (
     <header className="sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b bg-background/95 backdrop-blur transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
       <div className="flex h-4 items-center gap-2 px-4">
@@ -15,25 +19,7 @@ export const AppNav = () => {
       </div>
 
       <div className="ml-auto flex h-4 items-center gap-4 px-6">
-        <OrganizationSwitcher
-          hidePersonal
-          appearance={{
-            elements: {
-              organizationSwitcherTrigger:
-                'py-2 px-3 hover:bg-accent/50 rounded-md',
-            },
-          }}
-        />
-
-        <Separator orientation="vertical" className="mr-2 ml-2 h-4" />
-
-        <UserButton
-          appearance={{
-            elements: {
-              userButtonTrigger: 'hover:bg-accent/50',
-            },
-          }}
-        />
+        {user?.name}
       </div>
     </header>
   )
