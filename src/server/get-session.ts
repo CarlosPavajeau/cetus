64a1ea3cedb25ecc164a98e507d0670f2cccd501
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-07-19 11:18:43 -0500

    feat: add GetSession handler for user session retrieval

---
+++ src/server/get-session.ts
@@ -0,0 +1,16 @@
+import { auth } from '@/server/auth'
+import { createServerFn } from '@tanstack/react-start'
+import { getHeaders } from '@tanstack/react-start/server'
+
+export const GetSession = createServerFn({ method: 'GET' }).handler(
+  async () => {
+    const headers = new Headers(getHeaders() as HeadersInit)
+    const session = await auth.api.getSession({
+      headers,
+    })
+
+    return {
+      session: session?.session,
+    }
+  },
+)
