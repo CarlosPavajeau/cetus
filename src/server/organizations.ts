4f6257e91e12086868eb69f48dda08a6ba9e2eb8
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-07-19 22:17:05 -0500

    feat: add SetActiveOrg server function for managing active organizations

---
+++ src/server/organizations.ts
@@ -0,0 +1,28 @@
+import { createServerFn } from '@tanstack/react-start'
+import { getHeaders } from '@tanstack/react-start/server'
+import { auth } from './auth'
+
+export const SetActiveOrg = createServerFn({ method: 'POST' }).handler(
+  async () => {
+    const headers = new Headers(getHeaders() as HeadersInit)
+
+    const list = await auth.api.listOrganizations({
+      headers: headers,
+    })
+
+    if (list.length === 0) {
+      return undefined
+    }
+
+    const org = list.at(0)
+
+    await auth.api.setActiveOrganization({
+      body: {
+        organizationId: org?.id,
+      },
+      headers: headers,
+    })
+
+    return org?.id
+  },
+)
