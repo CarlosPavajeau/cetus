5147ee7d950d3e117ac6cff83746cae5172121c8
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-08-02 09:49:17 -0500

    feat: refactor store route to use tenant store for data fetching and remove unused API calls

--- src/routes/$store.tsx
+++ src/routes/$store.tsx
@@ -1,29 +1,34 @@
 import { fetchCategories } from '@/api/categories'
 import { fetchFeaturedProducts, fetchPopularProducts } from '@/api/products'
-import { fetchStoreBySlug } from '@/api/stores'
 import { DefaultPageLayout } from '@/components/default-page-layout'
 import { FeaturedProductsSection } from '@/components/home/featured-products-section'
 import { HeroSection } from '@/components/home/hero-section'
 import { HomeSkeleton } from '@/components/home/home-sekeleton'
 import { PopularProductsSection } from '@/components/home/popular-products-section'
 import { PageHeader } from '@/components/page-header'
-import { useAppStore } from '@/store/app'
-import { createFileRoute } from '@tanstack/react-router'
-import { useEffect } from 'react'
+import { useTenantStore } from '@/store/use-tenant-store'
+import { createFileRoute, notFound } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/$store')({
+  ssr: false,
   loader: async (context) => {
     const slug = context.params.store
-    const store = await fetchStoreBySlug(slug)
+    const { store } = useTenantStore.getState()
+    const { fetchAndSetStore } = useTenantStore.getState().actions
+
+    await fetchAndSetStore(slug)
+
+    if (!store) {
+      throw notFound()
+    }
 
     const [featuredProducts, popularProducts, categories] = await Promise.all([
-      fetchFeaturedProducts(store.slug),
-      fetchPopularProducts(store.slug),
-      fetchCategories(store.slug),
+      fetchFeaturedProducts(),
+      fetchPopularProducts(),
+      fetchCategories(),
     ])
 
     return {
-      store,
       featuredProducts,
       popularProducts,
       categories,
@@ -34,13 +39,8 @@ export const Route = createFileRoute('/$store')({
 })
 
 function RouteComponent() {
-  const { store, featuredProducts, popularProducts, categories } =
+  const { featuredProducts, popularProducts, categories } =
     Route.useLoaderData()
-  const appStore = useAppStore()
-
-  useEffect(() => {
-    appStore.setCurrentStore(store)
-  }, [])
 
   if (!categories || !featuredProducts || !popularProducts) {
     return (
