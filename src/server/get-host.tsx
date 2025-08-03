ad2b2d74252504e78d10d0de4e6c72605f02fe38
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-08-02 20:15:17 -0500

    fix: update getServerhost function to ensure proper header retrieval

--- src/server/get-host.tsx
+++ src/server/get-host.tsx
@@ -1,12 +1,15 @@
+import { env } from '@/shared/env'
 import { createServerFn } from '@tanstack/react-start'
 import { getHeader } from '@tanstack/react-start/server'
 
 export const getServerhost = createServerFn({ method: 'GET' }).handler(
   async () => {
     const host = getHeader('Host')!
+    const appUrl = new URL(env.APP_URL!)
 
     return {
       host,
+      isAppUrl: host === appUrl.host,
     }
   },
 )
