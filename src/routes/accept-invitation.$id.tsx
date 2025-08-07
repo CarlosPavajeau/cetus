3d049c733dd29cebb85c07a534db1ad97fb6dc71
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-08-07 14:20:07 -0500

    feat: update invitation acceptance UI with new layout and icons

--- src/routes/accept-invitation.$id.tsx
+++ src/routes/accept-invitation.$id.tsx
@@ -10,7 +10,7 @@ import {
   redirect,
   useNavigate,
 } from '@tanstack/react-router'
-import { HomeIcon, ShoppingCartIcon } from 'lucide-react'
+import { HomeIcon, HopIcon } from 'lucide-react'
 import { useState } from 'react'
 import { toast } from 'sonner'
 
@@ -42,41 +42,37 @@ export const Route = createFileRoute('/accept-invitation/$id')({
   errorComponent: ({ error }) => {
     if (error.message === 'You are not the recipient of the invitation') {
       return (
-        <div className="h-screen p-2">
-          <header className="lef-0 absolute top-0 z-30 w-full">
-            <div className="p-6 md:p-8">
-              <ShoppingCartIcon className="h-8 w-auto" />
-            </div>
-          </header>
+        <section className="flex min-h-screen bg-zinc-50 px-4 py-16 md:py-32 dark:bg-transparent">
+          <div className="m-auto h-fit w-full max-w-md overflow-hidden rounded-[calc(var(--radius)+.125rem)] border bg-muted dark:[--color-muted:var(--color-zinc-900)]">
+            <div className="-m-px rounded-[calc(var(--radius)+.125rem)] border bg-card p-8 pb-6">
+              <div className="text-center">
+                <Link
+                  aria-label="go home"
+                  className="mx-auto block w-fit"
+                  to="/"
+                >
+                  <HopIcon />
+                </Link>
+                <h1 className="mt-4 mb-1 font-semibold text-xl">
+                  No puedes aceptar esta invitación
+                </h1>
+                <p className="text-sm">
+                  No eres el destinatario de la invitación, verifica con la
+                  persona que te la envió.
+                </p>
+              </div>
 
-          <div className="flex h-full">
-            <div className="relative w-full">
-              <div className="relative z-10 flex h-full items-center justify-center p-6">
-                <div className="w-full max-w-md space-y-8">
-                  <div className="text-center">
-                    <h1 className="mb-4 font-heading text-lg">
-                      No puedes aceptar esta invitación
-                    </h1>
-
-                    <p className="mb-8 text-muted-foreground text-sm">
-                      No eres el destinatario de la invitación, verifica con la
-                      persona que te la envió.
-                    </p>
-                  </div>
-
-                  <div className="space-y-4">
-                    <Button asChild className="w-full">
-                      <Link to="/app">
-                        <HomeIcon />
-                        Ir al panel de control
-                      </Link>
-                    </Button>
-                  </div>
-                </div>
+              <div className="mt-6 space-y-6">
+                <Button asChild className="w-full">
+                  <Link to="/app">
+                    <HomeIcon />
+                    Ir al panel de control
+                  </Link>
+                </Button>
               </div>
             </div>
           </div>
-        </div>
+        </section>
       )
     }
 
@@ -127,51 +123,43 @@ function RouteComponent() {
   }
 
   return (
-    <div className="h-screen p-2">
-      <header className="lef-0 absolute top-0 z-30 w-full">
-        <div className="p-6 md:p-8">
-          <ShoppingCartIcon className="h-8 w-auto" />
-        </div>
-      </header>
+    <section className="flex min-h-screen bg-zinc-50 px-4 py-16 md:py-32 dark:bg-transparent">
+      <div className="m-auto h-fit w-full max-w-md overflow-hidden rounded-[calc(var(--radius)+.125rem)] border bg-muted dark:[--color-muted:var(--color-zinc-900)]">
+        <div className="-m-px rounded-[calc(var(--radius)+.125rem)] border bg-card p-8 pb-6">
+          <div className="text-center">
+            <Link aria-label="go home" className="mx-auto block w-fit" to="/">
+              <HopIcon />
+            </Link>
+            <h1 className="mt-4 mb-1 font-semibold text-xl">
+              Has sido invitado a ser parte de{' '}
+              <span className="font-semibold">
+                {invitation.organizationName}
+              </span>
+            </h1>
+            <p className="text-sm">
+              Acepta la invitación para unirte a la organización.
+            </p>
+          </div>
 
-      <div className="flex h-full">
-        <div className="relative w-full">
-          <div className="relative z-10 flex h-full items-center justify-center p-6">
-            <div className="w-full max-w-md space-y-8">
-              <div className="text-center">
-                <h1 className="mb-4 font-heading text-lg">
-                  Has sido invitado a ser parte de{' '}
-                  <span className="font-semibold">
-                    {invitation.organizationName}
-                  </span>
-                </h1>
-
-                <p className="mb-8 text-muted-foreground text-sm">
-                  Acepta la invitación para unirte a la organización.
-                </p>
-              </div>
-
-              <div className="grid grid-cols-2 gap-2">
-                <SubmitButton
-                  variant="secondary"
-                  isSubmitting={isLoading}
-                  disabled={isLoading}
-                  onClick={rejectInvitation}
-                >
-                  Rechazar invitación
-                </SubmitButton>
-                <SubmitButton
-                  isSubmitting={isLoading}
-                  disabled={isLoading}
-                  onClick={acceptInvitation}
-                >
-                  Aceptar invitación
-                </SubmitButton>
-              </div>
-            </div>
+          <div className="mt-6 grid grid-cols-2 gap-2">
+            <SubmitButton
+              disabled={isLoading}
+              isSubmitting={isLoading}
+              onClick={rejectInvitation}
+              variant="outline"
+            >
+              Rechazar invitación
+            </SubmitButton>
+            <SubmitButton
+              disabled={isLoading}
+              isSubmitting={isLoading}
+              onClick={acceptInvitation}
+            >
+              Aceptar invitación
+            </SubmitButton>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
