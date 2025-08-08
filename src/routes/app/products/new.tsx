12fc7fcf9c628809c3441801707ac4109ad1d8bc
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-08-07 22:18:04 -0500

    refactor: remove unnecessary fragment

--- src/routes/app/products/new.tsx
+++ src/routes/app/products/new.tsx
@@ -19,7 +19,7 @@ import type { FileWithPreview } from '@/hooks/use-file-upload'
 import { CreateProductSchema } from '@/schemas/product'
 import { arktypeResolver } from '@hookform/resolvers/arktype'
 import { createFileRoute } from '@tanstack/react-router'
-import { Fragment, useState } from 'react'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { v7 as uuid } from 'uuid'
 
@@ -78,27 +78,71 @@ function ProductCreateForm() {
   }
 
   return (
-    <Fragment>
-      <div className="space-y-4">
-        <h1 className="font-heading font-semibold text-2xl">Crear producto</h1>
+    <div className="space-y-4">
+      <h1 className="font-heading font-semibold text-2xl">Crear producto</h1>
 
-        <ReturnButton />
+      <ReturnButton />
 
-        <Form {...form}>
-          <form
-            onSubmit={handleSubmit}
-            className="space-y-8 rounded-md border border-muted bg-card p-4 md:p-8"
-          >
-            <div className="grid gap-4 md:grid-cols-2">
-              <div className="space-y-4">
+      <Form {...form}>
+        <form
+          className="space-y-8 rounded-md border border-muted bg-card p-4 md:p-8"
+          onSubmit={handleSubmit}
+        >
+          <div className="grid gap-4 md:grid-cols-2">
+            <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nombre</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Nombre del producto" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Descripción</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="Descripción del producto"
+                        {...field}
+                        value={field.value || ''}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <div className="grid grid-cols-2 gap-4">
                 <FormField
                   control={form.control}
-                  name="name"
+                  name="price"
                   render={({ field }) => (
                     <FormItem>
-                      <FormLabel>Nombre</FormLabel>
+                      <FormLabel>Precio</FormLabel>
                       <FormControl>
-                        <Input placeholder="Nombre del producto" {...field} />
+                        <div className="relative">
+                          <Input
+                            className="peer ps-6 pe-12"
+                            type="number"
+                            {...field}
+                          />
+                          <span className="pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 text-muted-foreground text-sm peer-disabled:opacity-50">
+                            $
+                          </span>
+                          <span className="pointer-events-none absolute inset-y-0 end-0 flex items-center justify-center pe-3 text-muted-foreground text-sm peer-disabled:opacity-50">
+                            COP
+                          </span>
+                        </div>
                       </FormControl>
                       <FormMessage />
                     </FormItem>
@@ -107,112 +151,64 @@ function ProductCreateForm() {
 
                 <FormField
                   control={form.control}
-                  name="description"
+                  name="stock"
                   render={({ field }) => (
                     <FormItem>
-                      <FormLabel>Descripción</FormLabel>
+                      <FormLabel>Stock</FormLabel>
                       <FormControl>
-                        <Textarea
-                          placeholder="Descripción del producto"
+                        <Input
+                          className="tabular-nums"
+                          type="number"
                           {...field}
-                          value={field.value || ''}
                         />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
                   )}
                 />
-
-                <div className="grid grid-cols-2 gap-4">
-                  <FormField
-                    control={form.control}
-                    name="price"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Precio</FormLabel>
-                        <FormControl>
-                          <div className="relative">
-                            <Input
-                              type="number"
-                              className="peer ps-6 pe-12"
-                              {...field}
-                            />
-                            <span className="pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 text-muted-foreground text-sm peer-disabled:opacity-50">
-                              $
-                            </span>
-                            <span className="pointer-events-none absolute inset-y-0 end-0 flex items-center justify-center pe-3 text-muted-foreground text-sm peer-disabled:opacity-50">
-                              COP
-                            </span>
-                          </div>
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-
-                  <FormField
-                    control={form.control}
-                    name="stock"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Stock</FormLabel>
-                        <FormControl>
-                          <Input
-                            type="number"
-                            className="tabular-nums"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-
-                <CategorySelector onSelectCreateCategory={openCategoryDialog} />
               </div>
 
-              <div>
-                <FormField
-                  control={form.control}
-                  name="images"
-                  render={() => (
-                    <FormItem>
-                      <FormLabel>Imágenes</FormLabel>
-                      <ProductImagesUploader
-                        onFilesChange={handleFilesChange}
-                      />
-
-                      <FormDescription>
-                        Las imágenes serán mostradas en el orden en que fueron
-                        subidas. Tomando como imagen principal la primera imagen
-                        subida.
-                      </FormDescription>
-
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
+              <CategorySelector onSelectCreateCategory={openCategoryDialog} />
             </div>
 
-            <div className="flex justify-end">
-              <SubmitButton
-                isSubmitting={createProductMutation.isPending}
-                disabled={createProductMutation.isPending}
-                className="w-full md:w-auto"
-              >
-                Crear producto
-              </SubmitButton>
-            </div>
-          </form>
-        </Form>
+            <div>
+              <FormField
+                control={form.control}
+                name="images"
+                render={() => (
+                  <FormItem>
+                    <FormLabel>Imágenes</FormLabel>
+                    <ProductImagesUploader onFilesChange={handleFilesChange} />
 
-        <CreateCategoryDialog
-          open={isCategoryDialogOpen}
-          onOpenChange={setIsCategoryDialogOpen}
-        />
-      </div>
-    </Fragment>
+                    <FormDescription>
+                      Las imágenes serán mostradas en el orden en que fueron
+                      subidas. Tomando como imagen principal la primera imagen
+                      subida.
+                    </FormDescription>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+
+          <div className="flex justify-end">
+            <SubmitButton
+              className="w-full md:w-auto"
+              disabled={createProductMutation.isPending}
+              isSubmitting={createProductMutation.isPending}
+            >
+              Crear producto
+            </SubmitButton>
+          </div>
+        </form>
+      </Form>
+
+      <CreateCategoryDialog
+        onOpenChange={setIsCategoryDialogOpen}
+        open={isCategoryDialogOpen}
+      />
+    </div>
   )
 }
