a94cf024cea9563b2cb8992bd05772818d2428be
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-08-10 10:16:55 -0500

    refactor: replace useStoreBySlug with fetchStoreByExternalId for store retrieval

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -1,17 +1,24 @@
-import { authClient } from '@/auth/auth-client'
+import { fetchStoreByExternalId } from '@/api/stores'
 import { AppNav } from '@/components/app-nav'
 import { AppSidebar } from '@/components/app-sidebar'
 import { MissingMercadoPagoConfigurationBanner } from '@/components/missing-mercado-pago-configuration-banner'
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
-import { useStoreBySlug } from '@/hooks/stores'
 import { GetSession } from '@/server/get-session'
 import { SetActiveOrg } from '@/server/organizations'
 import { useTenantStore } from '@/store/use-tenant-store'
+import { queryOptions, useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 import { useEffect } from 'react'
 
+const storeByExternalIdQuery = (id: string) =>
+  queryOptions({
+    queryKey: ['store', 'external', id],
+    queryFn: () => fetchStoreByExternalId(id),
+    staleTime: 1000 * 60 * 30, // 30 minutes
+  })
+
 export const Route = createFileRoute('/app')({
-  beforeLoad: async () => {
+  beforeLoad: async ({ context }) => {
     const { session, user } = await GetSession()
 
     if (!session) {
@@ -20,43 +27,41 @@ export const Route = createFileRoute('/app')({
       })
     }
 
-    if (!session.activeOrganizationId) {
+    let organizationId = session.activeOrganizationId
+    if (!organizationId) {
       const activeOrg = await SetActiveOrg()
       if (!activeOrg) {
         throw redirect({
           to: '/onboarding',
         })
       }
+
+      organizationId = activeOrg.id
     }
+
+    const store = await context.queryClient.ensureQueryData(
+      storeByExternalIdQuery(organizationId),
+    )
 
     return {
       user,
+      store,
+      organizationId,
     }
   },
-  shouldReload: false,
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  const { data: org, isPending } = authClient.useActiveOrganization()
-  const { store, isLoading } = useStoreBySlug(org?.slug)
-  const tenantStore = useTenantStore()
+  const { organizationId } = Route.useRouteContext()
+  const { data: store } = useSuspenseQuery(
+    storeByExternalIdQuery(organizationId),
+  )
+  const { actions } = useTenantStore()
 
   useEffect(() => {
-    if (isPending || isLoading) {
-      return
-    }
-
-    if (!org) {
-      return
-    }
-
-    if (!store) {
-      return
-    }
-
-    tenantStore.actions.setStore(store)
-  }, [isPending, org, store, isLoading])
+    actions.setStore(store)
+  }, [store, actions])
 
   return (
     <SidebarProvider>
