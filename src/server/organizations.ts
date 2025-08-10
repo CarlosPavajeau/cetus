6417d7a98b51c7482a40489fbd6dcfaac46b46be
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-08-09 23:32:49 -0500

    refactor: clean up organization handler by simplifying header usage and removing unnecessary checks

--- src/server/organizations.ts
+++ src/server/organizations.ts
@@ -1,26 +1,30 @@
+import { auth } from '@/server/auth'
 import { createServerFn } from '@tanstack/react-start'
 import { getHeaders } from '@tanstack/react-start/server'
-import { auth } from './auth'
 
 export const SetActiveOrg = createServerFn({ method: 'POST' }).handler(
   async () => {
     const headers = new Headers(getHeaders() as HeadersInit)
 
     const list = await auth.api.listOrganizations({
-      headers: headers,
+      headers,
     })
 
     if (list.length === 0) {
-      return undefined
+      return
     }
 
-    const org = list.at(0)!
+    const org = list.at(0)
+
+    if (!org) {
+      return
+    }
 
     await auth.api.setActiveOrganization({
       body: {
         organizationId: org.id,
       },
-      headers: headers,
+      headers,
     })
 
     return {
