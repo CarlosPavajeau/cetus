4b47881a46e71aee85b8cd84873fca62886e50b7
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-08-10 09:21:24 -0500

    refactor: streamline imports and remove unused route context in AppNav component

--- src/components/app-nav.tsx
+++ src/components/app-nav.tsx
@@ -1,20 +1,15 @@
 import { ThemeSwitch } from '@/components/theme-switch'
 import { Separator } from '@/components/ui/separator'
 import { SidebarTrigger } from '@/components/ui/sidebar'
-import { useRouteContext } from '@tanstack/react-router'
-import { UserMenu } from './user-menu'
+import { UserMenu } from '@/components/user-menu'
 
 export const AppNav = () => {
-  const { user } = useRouteContext({
-    from: '/app',
-  })
-
   return (
     <header className="sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b bg-background/95 backdrop-blur transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
       <div className="flex h-4 items-center gap-2 px-4">
         <SidebarTrigger className="-ms-2 hover:bg-accent/50" />
 
-        <Separator orientation="vertical" className="mr-2 ml-2 h-4" />
+        <Separator className="mr-2 ml-2 h-4" orientation="vertical" />
 
         <ThemeSwitch />
       </div>
