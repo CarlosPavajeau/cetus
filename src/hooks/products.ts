929b70dd4e4603872078b75c8c16fe44903136bf
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-09-28 22:04:51 -0500

    refactor: move useUpdateProduct hook to a separate file and clean up imports

--- src/hooks/products.ts
+++ src/hooks/products.ts
@@ -1,62 +1,10 @@
-import { uploadFileToS3 } from '@/api/aws'
 import {
   fetchProductBySlug,
   fetchProducts,
   fetchProductsForSale,
   fetchTopSellingProducts,
-  updateProduct,
 } from '@/api/products'
-import type { FileWithPreview } from '@/hooks/use-file-upload'
-import type { UpdateProduct } from '@/schemas/product'
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import consola from 'consola'
-import { toast } from 'sonner'
-
-export function useUpdateProduct(images: FileWithPreview[]) {
-  const queryClient = useQueryClient()
-
-  return useMutation({
-    mutationKey: ['products', 'update'],
-    mutationFn: async (values: UpdateProduct) => {
-      try {
-        const filesToUpload = values.images
-          .map((image) => {
-            const file = images.find((img) => img.id === image.id)
-
-            if (!file) {
-              return null
-            }
-
-            if (file.file instanceof File) {
-              return uploadFileToS3({
-                fileName: image.imageUrl,
-                file: file.file,
-              })
-            }
-
-            return null
-          })
-          .filter((upload) => upload !== null)
-
-        await Promise.all(filesToUpload)
-
-        return updateProduct(values)
-      } catch (error) {
-        consola.error('Failed to update product:', error)
-        const errorMessage =
-          error instanceof Error ? error.message : 'Error desconocido'
-        toast.error(`Error al actualizar el producto: ${errorMessage}`)
-        throw error
-      }
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: ['products'],
-      })
-      toast.success('Producto actualizado correctamente')
-    },
-  })
-}
+import { useQuery } from '@tanstack/react-query'
 
 export function useProductsForSale() {
   const { data, isLoading, error } = useQuery({
@@ -75,7 +23,6 @@ export function useProducts() {
   const { data, isLoading, error } = useQuery({
     queryKey: ['products'],
     queryFn: fetchProducts,
-    refetchOnMount: false,
   })
 
   return {
