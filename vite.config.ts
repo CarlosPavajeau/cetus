14c472943f0b74a6f1d92f89ce7f057cda2972b2
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-10-04 15:20:22 -0500

    chore: update dependencies and integrate nitro for improved performance

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
+import { nitro } from 'nitro-nightly/vite'
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
@@ -9,11 +10,10 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [
-    tsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
+    tsConfigPaths(),
     tailwindcss(),
     tanstackStart(),
+    nitro(),
     viteReact(),
   ],
 })
