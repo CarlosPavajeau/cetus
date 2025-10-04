0865ba6f4e4cabe0a6603d7b5e29b8ed8e4e2e56
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-10-04 09:50:19 -0500

    fix: update import statement for Slot component and adjust buttonVariants string formatting

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,11 +1,11 @@
-import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
+import { Slot } from 'radix-ui'
 import type * as React from 'react'
 
 import { cn } from '@/shared/cn'
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-[color,box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md font-medium text-sm outline-none transition-[color,box-shadow] focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
   {
     variants: {
       variant: {
@@ -34,24 +34,22 @@ const buttonVariants = cva(
   },
 )
 
-export type ButtonProps = React.ComponentPropsWithoutRef<'button'> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
-  }
-
 function Button({
   className,
   variant,
   size,
   asChild = false,
   ...props
-}: ButtonProps) {
-  const Comp = asChild ? Slot : 'button'
+}: React.ComponentProps<'button'> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean
+  }) {
+  const Comp = asChild ? Slot.Root : 'button'
 
   return (
     <Comp
-      data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
+      data-slot="button"
       {...props}
     />
   )
