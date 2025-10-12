722b0583367032756da7f7d4c85c69a555f38f76
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-10-12 11:07:44 -0500

    feat: implement CancelOrderDialog component with reason input and submission logic

---
+++ src/components/order/cancel-order-dialog.tsx
@@ -0,0 +1,113 @@
+import { cancelOrder } from '@/api/orders'
+import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Textarea } from '@/components/ui/textarea'
+import { arktypeResolver } from '@hookform/resolvers/arktype'
+import { useMutation } from '@tanstack/react-query'
+import { type } from 'arktype'
+import { BanIcon } from 'lucide-react'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { SubmitButton } from '../submit-button'
+
+type Props = {
+  orderId: string
+}
+
+const MaxCancelOrderReasonLength = 1024
+
+const CancelOrderSchema = type({
+  id: type('string.uuid'),
+  reason: type('string').lessThanLength(MaxCancelOrderReasonLength),
+})
+
+export function CancelOrderDialog({ orderId }: Readonly<Props>) {
+  const [open, setOpen] = useState(false)
+  const form = useForm({
+    resolver: arktypeResolver(CancelOrderSchema),
+    defaultValues: {
+      id: orderId,
+    },
+  })
+
+  const { mutateAsync } = useMutation({
+    mutationKey: ['orders', 'cancel', orderId],
+    mutationFn: cancelOrder,
+    onSuccess: (_, __, ___, context) => {
+      setOpen(false)
+      context.client.invalidateQueries({
+        queryKey: ['orders'],
+      })
+    },
+  })
+
+  const onSubmit = form.handleSubmit(async (values) => {
+    await mutateAsync(values)
+  })
+
+  return (
+    <Form {...form}>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button className="group w-full" size="lg" variant="destructive">
+            <BanIcon aria-hidden="true" size={16} />
+            Cancelar pedido
+          </Button>
+        </DialogTrigger>
+
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Cancelar pedido</DialogTitle>
+            <DialogDescription>
+              Por favor, indique el motivo del cancelamiento. Este motivo será
+              visible para el cliente y el administrador del sistema.
+            </DialogDescription>
+          </DialogHeader>
+
+          <form className="grid gap-4" onSubmit={onSubmit}>
+            <FormField
+              control={form.control}
+              name="reason"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Motivo</FormLabel>
+                  <FormControl>
+                    <Textarea {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter>
+              <SubmitButton
+                disabled={form.formState.isSubmitting}
+                isSubmitting={form.formState.isSubmitting}
+                variant="destructive"
+              >
+                Cancelar pedido
+              </SubmitButton>
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </Form>
+  )
+}
