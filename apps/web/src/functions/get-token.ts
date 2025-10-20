66b5d086a0f958b9744d1192527703843b2e4083
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-10-19 20:13:23 -0500

    refactor: convert get-token to a isomorphic function

--- apps/web/src/functions/get-token.ts
+++ apps/web/src/functions/get-token.ts
@@ -1,17 +1,23 @@
-import { createServerFn } from '@tanstack/react-start'
+import { auth } from '@cetus/auth'
+import { createIsomorphicFn } from '@tanstack/react-start'
 import { getRequestHeaders } from '@tanstack/react-start/server'
+import consola from 'consola'
 import { authClient } from '@/shared/auth-client'
 
-export const getToken = createServerFn({ method: 'GET' }).handler(async () => {
-  const response = await authClient.token({
-    fetchOptions: {
+export const getToken = createIsomorphicFn()
+  .server(async () => {
+    const { token } = await auth.api.getToken({
       headers: getRequestHeaders(),
-    },
+    })
+
+    return token
   })
+  .client(async () => {
+    const response = await authClient.token()
 
-  if (response.error) {
-    throw new Error(response.error.message)
-  }
+    if (response.error) {
+      consola.error(response.error)
+    }
 
-  return response.data.token
-})
+    return response.data?.token || ''
+  })
