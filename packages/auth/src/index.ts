633f087dc0a0ec03b350a637cab36ade2ee0185f
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-10-19 19:01:39 -0500

    chore: drop redirectURI

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -29,7 +29,6 @@ export const auth = betterAuth({
     google: {
       clientId: env.AUTH_GOOGLE_ID,
       clientSecret: env.AUTH_GOOGLE_SECRET,
-      redirectURI: `${env.APP_URL}/auth/google/callback`,
     },
   },
   plugins: [
