63523af9c9be5bfe388e091bb66b01d5814f7a8c
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-10-25 16:55:49 -0500

    feat: add HideableInput component for enhanced input visibility control

---
+++ apps/web/src/components/hideable-input.tsx
@@ -0,0 +1,34 @@
+import { EyeIcon, EyeOffIcon } from 'lucide-react'
+import { type ComponentProps, useState } from 'react'
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+} from './ui/input-group'
+
+type Props = ComponentProps<typeof InputGroupInput> & {
+  initialHidden?: boolean
+}
+
+export function HideableInput({ ...props }: Props) {
+  const [isHidden, setIsHidden] = useState(props.initialHidden ?? false)
+
+  return (
+    <InputGroup>
+      <InputGroupInput {...props} type={isHidden ? 'password' : 'text'} />
+      <InputGroupAddon align="inline-end">
+        <InputGroupButton
+          aria-label="input visibility"
+          onClick={() => {
+            setIsHidden((prev) => !prev)
+          }}
+          size="icon-xs"
+          title="input"
+        >
+          {isHidden ? <EyeIcon /> : <EyeOffIcon />}
+        </InputGroupButton>
+      </InputGroupAddon>
+    </InputGroup>
+  )
+}
