cbaa3cd81e509efe249323e7b169d21b4270bc30
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-10-26 11:21:28 -0500

    feat: include public key on financial institutions hook

--- apps/web/src/hooks/wompi/use-financial-institutions.ts
+++ apps/web/src/hooks/wompi/use-financial-institutions.ts
@@ -1,10 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
 import { getFinancialInstitutions } from '@/api/third-party/wompi'
 
-export const useFinancialInstitutions = () => {
+export const useFinancialInstitutions = (publicKey: string) => {
   const { data, isLoading } = useQuery({
     queryKey: ['financial-institutions'],
-    queryFn: getFinancialInstitutions,
+    queryFn: () => getFinancialInstitutions(publicKey),
   })
 
   return {
