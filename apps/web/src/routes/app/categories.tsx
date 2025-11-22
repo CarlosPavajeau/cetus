21fe32fafac381efc44020117bd7a639aff17d6e
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2025-11-22 10:46:08 -0500

    refactor(categories): simplify RouteComponent by integrating CategoriesTable and removing unused components

--- apps/web/src/routes/app/categories.tsx
+++ apps/web/src/routes/app/categories.tsx
@@ -1,62 +1,19 @@
-import type { Category } from '@/api/categories'
-import { ConfirmDeleteCategoryDialog } from '@/components/category/confirm-delete-category-dialog'
-import { EditCategoryDialog } from '@/components/category/edit-category-dialog'
-import { TablePagination } from '@/components/data-table/pagination'
-import { DataTable } from '@/components/data-table/table'
-import { DefaultLoader } from '@/components/default-loader'
-import { FormattedDate } from '@/components/formatted-date'
 import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-import { useTableWithPagination } from '@/hooks/use-table-with-pagination'
+import { CategoriesTable } from '@/features/categories/components/categories-table'
 import { CreateCategoryDialog } from '@cetus/web/features/categories/components/create-category.dialog'
 import { useCategories } from '@cetus/web/features/categories/hooks/use-categories'
 import { createFileRoute } from '@tanstack/react-router'
-import type { ColumnDef, Row } from '@tanstack/react-table'
-import { EllipsisIcon, PlusIcon } from 'lucide-react'
+import { PlusIcon } from 'lucide-react'
 import { useState } from 'react'
 
 export const Route = createFileRoute('/app/categories')({
   component: RouteComponent,
 })
 
-const columns: ColumnDef<Category>[] = [
-  {
-    id: 'name',
-    accessorKey: 'name',
-    header: 'Nombre',
-  },
-  {
-    id: 'createdAt',
-    accessorKey: 'createdAt',
-    header: 'Fecha de creación',
-    cell: ({ row }) => (
-      <div>
-        <FormattedDate date={new Date(row.getValue('createdAt'))} />
-      </div>
-    ),
-  },
-  {
-    id: 'actions',
-    header: () => <span className="sr-only">Actions</span>,
-    cell: ({ row }) => <RowActions row={row} />,
-    size: 60,
-    enableHiding: false,
-  },
-]
-
 function RouteComponent() {
   const { data, isLoading } = useCategories()
-  const { table, paginationInfo } = useTableWithPagination(columns, data ?? [])
 
   const [isOpenCreateCategory, setIsOpenCreateCategory] = useState(false)
-
-  if (isLoading) {
-    return <DefaultLoader />
-  }
 
   return (
     <div className="space-y-6">
@@ -77,36 +34,7 @@ function RouteComponent() {
         </Button>
       </div>
 
-      <div className="grid gap-4 overflow-hidden">
-        <DataTable table={table} />
-        <TablePagination paginationInfo={paginationInfo} table={table} />
-      </div>
+      <CategoriesTable categories={data} isLoading={isLoading} />
     </div>
-  )
-}
-
-function RowActions({ row }: Readonly<{ row: Row<Category> }>) {
-  const category = row.original
-
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <div className="flex justify-end">
-          <Button
-            aria-label="Edit item"
-            className="shadow-none"
-            size="icon"
-            variant="ghost"
-          >
-            <EllipsisIcon aria-hidden={true} size={16} />
-          </Button>
-        </div>
-      </DropdownMenuTrigger>
-
-      <DropdownMenuContent align="end">
-        <EditCategoryDialog category={category} />
-        <ConfirmDeleteCategoryDialog category={category} />
-      </DropdownMenuContent>
-    </DropdownMenu>
   )
 }
