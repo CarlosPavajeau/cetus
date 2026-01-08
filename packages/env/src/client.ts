544215a870fc80bc9f5964f04592e26d7f7ad10e
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-01-08 16:05:16 -0500

    chore: update environment variable type for VITE_APP_URL and simplify turbo.json task configurations

--- packages/env/src/client.ts
+++ packages/env/src/client.ts
@@ -8,7 +8,7 @@ export const env = createEnv({
     VITE_CDN_URL: type('string'),
     VITE_POSTHOG_KEY: type('string'),
     VITE_POSTHOG_HOST: type('string'),
-    VITE_APP_URL: type('string.url'),
+    VITE_APP_URL: type('string'),
   },
 
   // biome-ignore lint/suspicious/noExplicitAny: need to access import.meta.env
