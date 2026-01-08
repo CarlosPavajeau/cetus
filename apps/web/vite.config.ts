ba4125f032ef4de866a6c0c2a49610320d57f883
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-01-08 14:45:35 -0500

    chore: fix import name for tsconfigPaths and reorder server configuration

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,20 +3,18 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import { nitro } from 'nitro/vite'
 import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
   plugins: [
-    tsConfigPaths(),
+    tsconfigPaths(),
     tailwindcss(),
     tanstackStart(),
     nitro(),
     viteReact(),
   ],
-  nitro: {
-    preset: 'vercel',
+  server: {
+    port: 3000,
   },
+  nitro: {},
 })
