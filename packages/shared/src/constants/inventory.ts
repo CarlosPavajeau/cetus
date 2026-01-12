9e6d90bdef3ccc87db46e74b33d2540cdaa225c9
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-01-12 16:12:19 -0500

    feat: add inventory transaction type labels and colors constants

---
+++ packages/shared/src/constants/inventory.ts
@@ -0,0 +1,15 @@
+export const inventoryTransactionTypeLabels = Object.freeze({
+  sale: 'Venta',
+  adjustment: 'Ajuste',
+  return: 'Devolución',
+  purchase: 'Compra',
+  transfer: 'Transferencia',
+})
+
+export const inventoryTransactionTypeColors = Object.freeze({
+  sale: 'bg-success-base',
+  adjustment: 'bg-info-base',
+  return: 'bg-info-base',
+  purchase: 'bg-info-base',
+  transfer: 'bg-info-base',
+})
