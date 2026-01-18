41dd43e3a00e252ce936252dee35815509dcdee8
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-01-18 15:52:44 -0500

    refactor: reorganize OrderSummary layout for improved structure and readability

--- apps/web/src/features/orders/components/order-summary.tsx
+++ apps/web/src/features/orders/components/order-summary.tsx
@@ -1,6 +1,7 @@
 import type { Order } from '@cetus/api-client/types/orders'
 import { OrderCustomerCard } from '@cetus/web/features/orders/components/order-customer.card'
 import { OrderItemsList } from '@cetus/web/features/orders/components/order-items-list'
+import { OrderTimeline } from '@cetus/web/features/orders/components/order-timeline'
 import { PaymentSummary } from '@cetus/web/features/orders/components/payment-summary'
 
 type Props = {
@@ -9,18 +10,16 @@ type Props = {
 
 export function OrderSummary({ order }: Readonly<Props>) {
   return (
-    <div className="mt-8 grid grid-cols-1 gap-6 lg:grid-cols-12">
-      <div className="space-y-6 lg:col-span-12">
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-          <OrderCustomerCard
-            address={order.address}
-            customer={order.customer}
-          />
-
-          <PaymentSummary order={order} />
-        </div>
-
+    <div className="grid grid-cols-1 gap-4 xl:grid-cols-[1fr_320px]">
+      <div className="space-y-4">
         <OrderItemsList order={order} />
+      </div>
+      <div className="space-y-4">
+        <OrderCustomerCard address={order.address} customer={order.customer} />
+
+        <PaymentSummary order={order} />
+
+        <OrderTimeline order={order} />
       </div>
     </div>
   )
