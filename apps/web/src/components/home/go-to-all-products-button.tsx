e2988f773e17c143face7fe680e705a5f4f5c490
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-01-29 12:08:55 -0500

    refactor(syles): update go to all products button

--- apps/web/src/components/home/go-to-all-products-button.tsx
+++ apps/web/src/components/home/go-to-all-products-button.tsx
@@ -1,16 +1,17 @@
 import { Button } from '@cetus/ui/button'
+import { ArrowRight01Icon } from '@hugeicons/core-free-icons'
+import { HugeiconsIcon } from '@hugeicons/react'
 import { Link } from '@tanstack/react-router'
-import { ArrowRightIcon } from 'lucide-react'
 
 export function GoToAllProductsButton() {
   return (
     <Button asChild className="group" size="lg">
       <Link to="/products/all">
         Ver todos
-        <ArrowRightIcon
+        <HugeiconsIcon
           aria-hidden="true"
-          className="-me-1 opacity-60 transition-transform group-hover:translate-x-0.5"
-          size={16}
+          className="-me-1 h-4 w-4 opacity-60 transition-transform group-hover:translate-x-0.5"
+          icon={ArrowRight01Icon}
         />
       </Link>
     </Button>
