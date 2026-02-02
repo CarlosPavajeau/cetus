5fe1072ff3c5f579a79ca27788c5bafcb0023fe2
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-02-02 16:29:37 -0500

    feat: add find customer by phone endpoint

--- packages/api-client/src/endpoints/customers.ts
+++ packages/api-client/src/endpoints/customers.ts
@@ -3,4 +3,7 @@ import type { Customer } from '../types/customers'
 
 export const customersApi = {
   getById: (id: string) => anonymousClient.get<Customer>(`/customers/${id}`),
+
+  getByPhone: (phone: string) =>
+    anonymousClient.get<Customer>(`/customers/by-phone/${phone}`),
 }
