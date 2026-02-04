5e3b06b088651471e1fd2732eb259724549155b2
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-02-04 14:24:38 -0500

    fix: update separator styles

--- apps/web/src/components/app-nav.tsx
+++ apps/web/src/components/app-nav.tsx
@@ -4,13 +4,10 @@ import { ThemeSwitch } from '@cetus/web/components/theme-switch'
 
 export const AppNav = () => (
   <header className="flex h-16 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-    <div className="flex items-center gap-2 px-4">
+    <div className="flex h-4 items-center gap-2 px-4">
       <SidebarTrigger className="-ml-1" />
 
-      <Separator
-        className="mr-2 data-[orientation=vertical]:h-4"
-        orientation="vertical"
-      />
+      <Separator className="h-4" orientation="vertical" />
 
       <ThemeSwitch />
     </div>
