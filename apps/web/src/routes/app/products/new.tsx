17074023f96ddb0b8f8955f56829fcc1dd30a060
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-02-06 21:47:29 -0500

    feat: add info tooltip on register product form

--- apps/web/src/routes/app/products/new.tsx
+++ apps/web/src/routes/app/products/new.tsx
@@ -1,7 +1,19 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@cetus/ui/tabs'
 import { ReturnButton } from '@cetus/web/components/return-button'
+import { Button } from '@cetus/web/components/ui/button'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@cetus/web/components/ui/tooltip'
 import { AdvancedProductRegistrationForm } from '@cetus/web/features/products/components/advanced-product-registration-form'
 import { SimpleProductRegistrationForm } from '@cetus/web/features/products/components/simple-product-registration-form'
+import {
+  InformationCircleIcon,
+  Settings01Icon,
+  ZapIcon,
+} from '@hugeicons/core-free-icons'
+import { HugeiconsIcon } from '@hugeicons/react'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/app/products/new')({
@@ -17,11 +29,31 @@ function CreateProductPage() {
 
         <h1 className="font-heading font-medium text-lg">Registrar producto</h1>
 
-        <Tabs defaultValue="simple">
-          <TabsList>
-            <TabsTrigger value="simple">Registro simple</TabsTrigger>
-            <TabsTrigger value="advanced">Registro avanzado</TabsTrigger>
-          </TabsList>
+        <Tabs className="gap-3" defaultValue="simple">
+          <div className="flex items-center gap-2">
+            <TabsList>
+              <TabsTrigger value="simple">
+                <HugeiconsIcon icon={ZapIcon} />
+                Registro simple
+              </TabsTrigger>
+              <TabsTrigger value="advanced">
+                <HugeiconsIcon icon={Settings01Icon} />
+                Registro avanzado
+              </TabsTrigger>
+            </TabsList>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button size="icon-sm" variant="ghost">
+                  <HugeiconsIcon icon={InformationCircleIcon} />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                Usa el registro avanzado para registrar productos que tengan
+                variantes o múltiples opciones.
+              </TooltipContent>
+            </Tooltip>
+          </div>
 
           <TabsContent value="simple">
             <SimpleProductRegistrationForm />
