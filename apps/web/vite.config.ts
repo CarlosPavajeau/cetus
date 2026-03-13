1d2c07e8f39193f29ea133b0dee1c5fd5bc37a30
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-03-13 15:08:39 -0500

    fix: update vite configuration

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,19 +4,14 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import { nitro } from 'nitro/vite'
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [
-    devtools(),
-    tsconfigPaths(),
-    tailwindcss(),
-    tanstackStart(),
-    nitro(),
-    viteReact(),
-  ],
+  nitro: {},
   server: {
     port: 3000,
   },
-  nitro: {},
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [devtools(), tailwindcss(), tanstackStart(), nitro(), viteReact()],
 })
